a2c94650c03b991a73c0be5888f4c0d6f0d8d4ce
Ae-Mc <ae_mc@mail.ru>
2020-10-01 15:51:15 +0300

    Follow PEP 557 standard for dataclass fields

--- FB2/FictionBook2.py
+++ FB2/FictionBook2.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 from typing import List, Optional, Tuple
 from uuid import uuid4
@@ -7,8 +7,8 @@ from .FB2Builder import FB2Builder
 
 @dataclass
 class FictionBook2():
-    genres: List[str] = ()
-    authors: List[str] = ()
+    genres: List[str] = field(default_factory=list)
+    authors: List[str] = field(default_factory=list)
     title: str = ""
     annotation: Optional[str] = None
     keywords: Optional[List[str]] = None
@@ -18,10 +18,10 @@ class FictionBook2():
     srcLang: Optional[str] = None
     translators: Optional[List[str]] = None
     sequences: Optional[List[Tuple[str, int]]] = None
-    docAuthors: List[str] = tuple("Ae-Mc")
+    docAuthors: List[str] = field(default_factory=list)
     docId: str = str(uuid4())
     docVersion: str = "1.0"
-    chapters: List[Tuple[str, List[str]]] = ()
+    chapters: List[Tuple[str, List[str]]] = field(default_factory=list)
 
     def write(self, filename: str):
         with open(filename, 'w') as f:
