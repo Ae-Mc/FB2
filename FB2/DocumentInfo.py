5aa1b217206bbc0f72e094a175210129e7c1efb7
Ae-Mc <ae_mc@mail.ru>
2020-10-04 00:25:39 +0300

    Write DocumentInfo data class

---
+++ FB2/DocumentInfo.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from .Author import Author
+from typing import List, Union, Optional, Tuple
+from datetime import datetime
+from uuid import uuid4
+import xml.etree.ElementTree as ET
+from decimal import Decimal
+
+
+@dataclass
+class DocumentInfo:
+    authors: List[Union[Author, str]] = field(default_factory=list)
+    programUsed: Optional[str] = "Ae-Mc's FB2 library"
+    date: Tuple[datetime, Optional[str]] = (datetime.now(), None)
+    sourceUrl: Optional[str] = None
+    sourceAuthor: Optional[Union[Author, str]] = None
+    id: str = str(uuid4())
+    version: Decimal = Decimal('1.0')
+    history: Optional[Union[str, ET.Element]] = None
+    publisher: Optional[Union[Author, str]] = None
